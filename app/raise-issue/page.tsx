e9ad6275a3236a4f69547d3a1efc5bd67f7dacbd
KrishnaPrasad-dev <itzmekrishna.257@gmail.com>
2026-02-02 10:55:54 +0530

    Spaces changed in placeholders

--- app/raise-issue/page.tsx
+++ app/raise-issue/page.tsx
@@ -128,7 +128,7 @@ export default function RaiseIssuePage() {
                 required
                 value={form.title}
                 onChange={handleChange}
-                className="w-full pl-10 pr-4 py-2.5 rounded-lg bg-slate-900 border border-slate-700 text-white focus:ring-2 focus:ring-indigo-600"
+                className="w-full pr-4 py-2.5 rounded-lg bg-slate-900 border border-slate-700 text-white focus:ring-2 focus:ring-indigo-600"
               />
             </div>
           </div>
@@ -147,7 +147,7 @@ export default function RaiseIssuePage() {
                 placeholder="Enter the issue description here"
                 value={form.description}
                 onChange={handleChange}
-                className="w-full pl-10 pr-4 py-2.5 rounded-lg bg-slate-900 border border-slate-700 text-white focus:ring-2 focus:ring-indigo-600 resize-none"
+                className="w-full  pr-4 py-2.5 rounded-lg bg-slate-900 border border-slate-700 text-white focus:ring-2 focus:ring-indigo-600 resize-none"
               />
             </div>
           </div>
